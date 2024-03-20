649faa88014c2f0fb2b1016aefb8bc091c3ad833
heytroys <150818764+heytroys@users.noreply.github.com>
2024-03-20 11:08:42 +0900

    Update touitu.js

--- assets/js/touitu.js
+++ assets/js/touitu.js
@@ -11,22 +11,25 @@
     <ul id="nav" class="sports-list d-flex list-unstyled pt-2">
       <li class="pe-3"><a href="#" class="sub-text fs-3 text-dark text-decoration-none">baseball</a>
       <ul class="list-unstyled">
-        <li class="my-4"><a class="fs-5 text-dark text-decoration-none" href="events/baseball-tiger24-1-op.html">阪神タイガース
-          <br>1軍オープン戦2024</a></li>
-        <li class="my-4"><a class="fs-5 text-dark text-decoration-none" href="events/th-other-baseball24-1-op.html">阪神以外
-          <br>1軍オープン戦2024</a></li>
+        <li class="my-4"><a class="fs-6 text-dark text-decoration-none" href="events/baseball-tiger24-1-op.html">阪神タイガース
+          <br>1軍オープン戦<br>
+          2024</a></li>
+        <li class="my-4"><a class="fs-6 text-dark text-decoration-none" href="events/th-other-baseball24-1-op.html">阪神以外
+          <br>1軍オープン戦
+          <br>2024</a></li>
         </ul>
       </li>
       <li class="pe-3"><a href="#" class="sub-text fs-3 text-dark text-decoration-none">volleyball</a>
         <ul class="list-unstyled">
-          <li class="my-4"><a class="sub-text fs-5 text-dark text-decoration-none" href="events/jtmave23.html">JTマーヴェラス
-            <br>2023~2024</a></li>
+          <li class="my-4"><a class="sub-text fs-6 text-dark text-decoration-none" href="events/jtmave23.html">JTマーヴェラス
+            <br>2023~24</a></li>
           </ul>
         </li>
         <li class="pe-3"><a href="#" class="sub-text fs-3 text-dark text-decoration-none">figureskate</a>
           <ul class="list-unstyled">
-            <li class="my-4"><a class="sub-text fs-5 text-dark text-decoration-none" href="events/zenja-figskate23.html">2023全日本
-              <br>フィギアスケート選手権</a></li>
+            <li class="my-4"><a class="sub-text fs-6 text-dark text-decoration-none" href="events/zenja-figskate23.html">2023全日本
+              <br>フィギアスケート<br>
+              選手権</a></li>
             </ul>
           </li>
       <li class="pe-2"><a href="events/prof.html" class="fs-3 text-dark text-decoration-none">profile</a></li>
