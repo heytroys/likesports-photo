ae7106bb82188962dec34bdb5372dad5db7f812d
heytroys <150818764+heytroys@users.noreply.github.com>
2024-04-15 12:31:05 +0900

    Update touitu.js

--- assets/js/touitu.js
+++ assets/js/touitu.js
@@ -24,13 +24,6 @@
             <br>2023~24</a></li>
           </ul>
         </li>
-        <li class="pe-3"><a class="sub-text fs-3 text-dark text-decoration-none">figureskate</a>
-          <ul class="list-unstyled">
-            <li class="my-4"><a class="sub-text fs-6 text-dark text-decoration-none" href="events/zenja-figskate23.html">2023全日本
-              <br>フィギアスケート<br>
-              選手権</a></li>
-            </ul>
-          </li>
       <li class="pe-2"><a href="events/prof.html" class="fs-3 text-dark text-decoration-none">profile</a></li>
     </ul>
   </nav>
